aa8efe0215919d47483702a6113ddf36a4b86b40
Hieu Le <minhhieule02@gmail.com>
2024-03-07 18:30:25 +0000

    Backend: Created API Endpoints
    /upload - POST -  Upload images
    /addproduct - POST - Upload product to DB
    /removeproduct - POST - Delete product
    /allproducts - GET - Get all products

--- backend/index.js
+++ backend/index.js
@@ -6,23 +6,123 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer"); 
 const path = require("path");
 const cors = require("cors");
-const { error } = require("console");
+const { error, log } = require("console");
 
 
 app.use(express.json()); //Request from response automatically passed through json
-app.use(cors); //ReactJS project connect to express app on port 4000
+app.use(cors()); //ReactJS project connect to express app on port 4000
 
 //Database connection with MongoDB
 mongoose.connect("***REMOVED***");
 
-//API endpoint creation
+//API Endpoint: Create root
 app.get("/",(req,res)=>{
     res.send("Express App is running");
 })
 
+//Image storage engine
+const storage = multer.diskStorage({ //Disk storage configuration
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({storage:storage});
+
+//API Endpoints: Upload images
+app.use('/images',express.static('upload/images')) //provide image url
+app.post("/upload",upload.single('product'),(req,res) => {
+    res.json({
+        success:1, //if image successfully uploaded, will return success:1
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+//Schema for Creating Products
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    new_price:{
+        type: Number,
+        required: true,
+    },
+    old_price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    },
+    available:{
+        type: Boolean,
+        default: true,
+    },
+})
+
+
+//API Endpoint: Create product using db schema and save to database
+app.post('/addproduct',async(req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length > 0){ //Create new id by increament
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
+    } else {
+        id = 1;  
+    }
+    const product = new Product({
+        id: id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+    console.log(product);
+    await product.save(); //Save in mongodb
+    console.log("Saved");
+    res.json({ //Response for frontend
+        success: true,
+        name: req.body.name,
+    })
+})
+
+//API Endpoint: delete products
+app.post('/removeproduct', async(req,res)=>{ //Endpoint
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
+})
+
+//API Endpoint: Get all products
+app.get('/allproducts', async(req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
+
 app.listen(port,(error)=>{
     if (!error){
-        console.log("Server running on port" + port);
+        console.log("Server running on port " + port);
     } 
     else {
         console.log("Error: " + error);
