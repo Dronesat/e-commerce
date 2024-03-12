00231935affc6c28767d2bb37f2e76844ee00568
Hieu Le <minhhieule02@gmail.com>
2024-03-12 17:37:35 +0000

    Backend: Created API Endpoints (/login , /signup)
    /signup - POST - Registering the user
    /login - POST - User login

--- backend/index.js
+++ backend/index.js
@@ -120,6 +120,85 @@ app.get('/allproducts', async(req,res)=>{
     res.send(products);
 })
 
+//Schema for creating User model
+const Users = mongoose.model('Users',{
+    name:{
+        type: String,
+    },
+    email:{
+        type: String,
+        unique: true,
+    },
+    password:{
+        type: String,
+    },
+    cartData:{
+        type: Object,
+    },
+    date:{
+        type: Date,
+        default: Date.now(),
+    }
+})
+
+//API Endpoint: Registering the user
+app.post('/signup',async(req,res)=>{
+    //Check if email already existed
+    let check = await Users.findOne({email:req.body.email});
+    if (check) {
+        return res.status(400).json({success:false,error:"Existing user found with the same email address"})
+    }
+    
+    //If no user, create 300 empty carts
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i]=0;
+    }
+
+    //Create user
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    })
+
+    await user.save(); //Save user in database
+
+    //Create token using jwt from data object
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token});
+})
+
+//API Endpoint: User login
+app.post('/login',async(req,res)=>{
+    //Find user
+    let user = await Users.findOne({email: req.body.email});
+    if (user) {
+        //passCompare true if password existed then create new data object(user.id)
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            //Password correct generates one token
+            const token = jwt.sign(data,'secrete_ecom');
+            res.json({success:true,token});
+        } else {
+            res.json({success:false,errors:"Wrong Password"});
+        }
+    } else {
+        res.json({success:false,errors:"Wrong Email Address"})
+    }
+})
+
 app.listen(port,(error)=>{
     if (!error){
         console.log("Server running on port " + port);
